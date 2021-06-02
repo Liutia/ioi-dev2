403fcfe981c9d0ff43e1c5e7f9fd7ee2ecfd1054
RomaRoma49 <v12nes11@gmail.com>
2021-06-02 17:12:29 +0300

    tsk12 - some JS code refactor

--- web/themes/custom/IOI_theme/js/custom.js
+++ web/themes/custom/IOI_theme/js/custom.js
@@ -8,32 +8,30 @@
   'use strict';
 
   Drupal.behaviors.IOI_theme = {
-    attach: function(context, settings) {
-
+    attach: function() {
       window.onscroll = function() {scrollFunction()};
 
       function scrollFunction() {
         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-          $("#navbar-main").css("height", "60px");
-          $("#navbar-main").css("background", "white");
-          $("#navbar-main").css("border-bottom", "none");
-          $("#block-headerlogo").css("display", "none");
+          $("#navbar-main").css({
+            height: '60px',
+            background: 'white',
+            borderBottom: 'none',
+          });
           $("#block-headerlogoonscroll").css("display", "block");
           $('.nav-link').css("color", "black");
         } else {
-          $("#navbar-main").css("height", "100px");
-          $("#navbar-main").css("border-bottom", "1px solid white");
-          $("#navbar-main").css("background", "rgba(0, 0, 0, 0)");
+          $("#navbar-main").css({
+            height: '100px',
+            borderBottom: '1px solid white',
+            background: 'rgba(0, 0, 0, 0)',
+          });
           $("#block-headerlogoonscroll").css("display", "none");
           $("#block-headerlogo").css("display", "block");
           $('.nav-link').css("color", "white");
-
         }
       }
     }
   };
 
 })(jQuery, Drupal);
-
-
-
