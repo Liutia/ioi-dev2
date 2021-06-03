26553f034d3681f19251dd0a33044ccaa86477a9
RomaRoma49 <74567713+RomaRoma49@users.noreply.github.com>
2021-06-03 12:38:28 +0300

    Update custom.js

--- web/themes/custom/IOI_theme/js/custom.js
+++ web/themes/custom/IOI_theme/js/custom.js
@@ -14,16 +14,20 @@
 
       function scrollFunction() {
         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-          $("#navbar-main").css("height", "60px");
-          $("#navbar-main").css("background", "white");
-          $("#navbar-main").css("border-bottom", "none");
+          $("#navbar-main").css({
+            height: '60px',
+            background: 'white',
+            borderBottom: 'none',
+          });
           $("#block-headerlogo").css("display", "none");
           $("#block-headerlogoonscroll").css("display", "block");
           $('.nav-link').css("color", "black");
         } else {
-          $("#navbar-main").css("height", "100px");
-          $("#navbar-main").css("border-bottom", "1px solid white");
-          $("#navbar-main").css("background", "rgba(0, 0, 0, 0)");
+          $("#navbar-main").css({
+            height: '100px',
+            borderBottom: '1px solid white',
+            background: 'rgba(0, 0, 0, 0)',
+          });
           $("#block-headerlogoonscroll").css("display", "none");
           $("#block-headerlogo").css("display", "block");
           $('.nav-link').css("color", "white");
@@ -34,6 +38,4 @@
   };
 
 })(jQuery, Drupal);
-
-
 
